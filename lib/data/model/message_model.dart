5b8a589342710f76174d5e06b3ef6212e22e9bd0
prosenjit <prosenjitsarkerr.cse@gamil.com>
2026-04-28 20:39:16 +0600

    constant color and strin added

---
+++ lib/data/model/message_model.dart
@@ -0,0 +1,19 @@
+import '../../domain/entities/message_entity.dart';
+
+class MessageModel extends MessageEntity {
+  MessageModel({required super.role, required super.text, required super.time});
+  // sample -- > ui without macking a real api call
+  factory MessageModel.sampleUser() => MessageModel(
+    role: 'user',
+    text: 'Hello! what can you help me with?',
+    time: DateTime.now(),
+  );
+  factory MessageModel.sampleBot() => MessageModel(
+    role: 'assistant',
+    text: 'Hi there! I am you AI assistant. Ask me anything',
+    time: DateTime.now(),
+  );
+  // Converts to the format that api expects
+  Map<String, String> toApiMap()=> {'role': role, 'content': text,
+    'model': 'gpt-3.5-turbo'};
+}
